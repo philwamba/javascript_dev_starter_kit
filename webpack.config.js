4a9ca18791d00d74f7db816200bdd5664effdd44
Phil Wamba <philwamba@gmail.com>
2020-01-04 13:56:54 +0300

    Add webpack & babel

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: ["@babel/polyfill", "./src"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
